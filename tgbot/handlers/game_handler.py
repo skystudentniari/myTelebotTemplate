91387a637b0959e45c2ece6f8df21a2a6ee820ef
Roman <35300872+monaxovdulov@users.noreply.github.com>
2024-08-14 16:47:12 +0300

    fix bug

--- tgbot/handlers/game_handler.py
+++ tgbot/handlers/game_handler.py
@@ -8,12 +8,13 @@ from ..services.game_logic import determine_winner, computer_move, player_move
 MAY_CHOICE = ["камень", "ножницы", "бумага"]
  
 def start_game_with_bot(message: Message, bot: TeleBot):
-    user_id = messsage.from_user.id
+    user_id = message.from_user.id
     bot.send_message(user_id, f"Привет, выберите ход: (камень/ножницы/бумага)") #move
     bot.register_next_step_handler(message, process_bot_choice, bot)
     
     
 def process_bot_choice(message: Message, bot: TeleBot):
+    user_id = message.from_user.id
     player_choice = message.text
     bot_choice = random.choice(MAY_CHOICE)
     player1_info = {"name": message.from_user.first_name, "move": player_choice}
